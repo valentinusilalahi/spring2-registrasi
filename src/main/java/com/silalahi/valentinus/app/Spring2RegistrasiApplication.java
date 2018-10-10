15d49faf8ce75d9e82ffaf63388c96c3654e310b
valentinus silalahi <valentinus.silalahi@gmail.com>
2018-10-10 16:46:11 +0700

    first commit

---
+++ src/main/java/com/silalahi/valentinus/app/Spring2RegistrasiApplication.java
@@ -0,0 +1,12 @@
+package com.silalahi.valentinus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2RegistrasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2RegistrasiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/silalahi/valentinus/app/Spring2RegistrasiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silalahi.valentinus.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring2RegistrasiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
