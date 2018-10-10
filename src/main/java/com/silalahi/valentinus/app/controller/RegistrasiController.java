7a6f51b879f8935af1c06a68da41a959be6dd38e
valentinus silalahi <valentinus.silalahi@gmail.com>
2018-10-10 17:41:50 +0700

    2. controller and model

--- src/main/java/com/silalahi/valentinus/app/controller/RegistrasiController.java
+++ src/main/java/com/silalahi/valentinus/app/controller/RegistrasiController.java
@@ -1,18 +1,41 @@
 package com.silalahi.valentinus.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mkopylec.recaptcha.validation.RecaptchaValidator;
+import com.github.mkopylec.recaptcha.validation.ValidationResult;
+import com.silalahi.valentinus.app.dto.Registrasi;
 
 @Controller
 public class RegistrasiController {
-	
-	@GetMapping("/registrasi/form")
-	public void formDaftar() {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegistrasiController.class);
+
+	@Autowired
+	private RecaptchaValidator recaptchaValidator;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@PostMapping("/registrasi")
+	public String prosesRegistrasi(@ModelAttribute @Valid Registrasi reg, BindingResult errors, SessionStatus status,
+			HttpServletRequest request) {
 		
-	}
-	
-	@GetMapping("/registrasi/formregistrasiawal")
-	public void formAwal() {
-		
+		ValidationResult hasil = recaptchaValidator.validate(request);
+		if(!hasil.isSuccess()) {
+			LOGGER.warn("Cap");
+		}
+
 	}
 }
